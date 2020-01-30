e163c47a086afe4d36036bb000b7ecca284d717d
Mikhail Danshin <effectdopler0@gmail.com>
2020-01-30 10:46:59 +0300

    создал и подключил Loader

--- src/store/info.js
+++ src/store/info.js
@@ -17,7 +17,9 @@ export default {
           const uid = await dispatch('getUid')
           const info = (await firebase.database().ref(`/users/${uid}/info`).once('value')).val()
           commit('setInfo', info)
-        } catch(e) {}
+        } catch (e) {
+
+        }
       }
     }
   }, 
